42f9893df1fe98e958b466e35b528298a22679a5
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-11-02 22:40:27 +0000

    styled search icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
@@ -18,7 +18,7 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className='text-red-500'></Text>
 
       {/* Header */}
@@ -29,13 +29,26 @@ const HomeScreen = () => {
         }}
         className='h-7 w-7 bg-gray-300 rounded-full'
         />
-        <View>
+        <View className='flex-1'>
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
           <Text className='font-bold text-xl'>Current Location
             <ChevronDownIcon size={20} color="#00CCBB"/>
           </Text>
         </View>
+          <UserIcon size={35} color="#00CCBB"/>
       </View>
+
+      {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+           <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+            <MagnifyingGlassIcon color='gray' size={20} />
+            <TextInput
+            placeholder="Restuarants and cusinies"
+            keyboardType='default'
+            />
+           </View>
+          <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
     </SafeAreaView>
   )
 }
