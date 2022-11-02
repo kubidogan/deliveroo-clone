a22a3d5a90f5c83c838bad5daff2cbdfe630f121
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-11-02 12:07:42 +0000

    babel and react-navigation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content:
+  [
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+};
