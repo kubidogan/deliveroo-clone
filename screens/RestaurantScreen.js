a38eebf53118e535af60c9b56059c3ea32038b0b
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-11-26 19:41:20 +0000

    added back button to rest screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,6 +1,8 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { urlFor } from '../sanity';
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -28,13 +30,18 @@ const RestaurantScreen = () => {
 
   return (
     <ScrollView>
-      <View>
+      <View className='relative'>
         <Image
-          soruce={{
-            uri: urlFor(imgUrl).url(),
-          }}
-          className="w-full h-56 bg-gray-300 p-4"
-        />
+            source={{
+              uri: urlFor(imgUrl).url(),
+            }}
+            className="w-full h-56 bg-gray-300 p-4"
+          />
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className='absolute top-14 left-5 p-2 bg-gray-100 rounded-full'>
+            <ArrowLeftIcon size={20} color="#00CCBB" />
+          </TouchableOpacity>
       </View>
     </ScrollView>
   );
