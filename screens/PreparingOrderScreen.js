fbd38bed25aeb74b85b70a0aae18a0d917dff469
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-11-27 23:11:58 +0000

    created preparing order screen

---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const PreparingOrderScreen = () => {
+  return (
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
+      <View>
+        <Text>PreparingOrderScreen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default PreparingOrderScreen
