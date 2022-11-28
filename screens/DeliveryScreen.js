6a26b9e853998c80d846746a987fd0bb4ea9eaff
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-11-28 00:47:49 +0000

    app complete

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { XCircleIcon } from "react-native-heroicons/solid";
 import * as Progress from 'react-native-progress';
+import MapView, { Marker } from "react-native-maps";
 
 const DeliveryScreen = () => {
   const navigation = useNavigation();
@@ -27,17 +28,56 @@ const DeliveryScreen = () => {
             </View>
             <Image
               source={{
-                uri: "https://links.papareact.com/fls"
+                uri: "https://links.papareact.com/fls",
               }}
               className="h-20 w-20"
             />
             </View>
 
             <Progress.Bar size={30} color="00CCBB" indeterminate={true} />
+
             <Text className="mt-3 text-gray-500">
               Your order at {restaurant.title} is being prepared
             </Text>
         </View>
+      </SafeAreaView>
+
+      <MapView
+      initialRegion={{
+        latitude: restaurant.lat,
+        longitude: restaurant.long,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+      className="flex-1 -mt-10 z-0"
+      mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+          }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          idenifier="origin"
+          pinColor="#00CCBB"
+        />
+      </MapView>
+
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+          />
+          <View className="flex-1">
+            <Text className="text-lg">
+              Kubilay Dogan
+            </Text>
+            <Text className="text-gray-400 ">Your Rider</Text>
+          </View>
+          <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
       </SafeAreaView>
     </View>
   )
